b1b7f58ff5b819fec2dea486d6dfadf7f62ddaa6
danesh kabade <daneshvkaade@gmail.com>
2026-06-06 00:46:14 +0530

    springboot enterprise app initial commit

---
+++ src/main/java/com/enterprise/app/Application.java
@@ -0,0 +1,24 @@
+package com.enterprise.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "Spring Boot Application Running on AWS EKS 🚀";
+    }
+
+    @GetMapping("/health")
+    public String health(){
+        return "Application Healthy";
+    }
+}
